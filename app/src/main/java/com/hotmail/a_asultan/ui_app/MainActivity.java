2b66dab0161036d9bec1952b8b3df0a008e4180f
sultan012 <a_asultan@hotmail.com>
2019-05-03 14:45:24 +0200

    commit

---
+++ app/src/main/java/com/hotmail/a_asultan/ui_app/MainActivity.java
@@ -0,0 +1,173 @@
+package com.hotmail.a_asultan.ui_app;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.Layout;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    // An int variable to hold a value
+    private int value = 0;
+
+    // A bunch of Buttons and a TextView
+    private Button btnAdd;
+    private Button btnTake;
+    private TextView txtValue;
+    private Button btnGrow;
+    private Button btnShrink;
+    private Button btnReset;
+    private Button btnHide;
+    private Button btnColor;
+    private CoordinatorLayout test;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Get a reference to all the buttons in our UI
+        // Match them up to all our Button objects we declared earlier
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnTake = (Button) findViewById(R.id.btnTake);
+        txtValue = (TextView) findViewById(R.id.textValue);
+        btnGrow = (Button) findViewById(R.id.btnGrow);
+        btnShrink = (Button) findViewById(R.id.btnShrink);
+        btnReset = (Button) findViewById(R.id.btnReset);
+        btnHide = (Button) findViewById(R.id.btnHide);
+        btnColor = (Button) findViewById(R.id.btnColor);
+        test = (CoordinatorLayout) findViewById(R.id.test);
+
+        // Listen for all the button clicks
+        btnAdd.setOnClickListener(this);
+        btnTake.setOnClickListener(this);
+        txtValue.setOnClickListener(this);
+        btnGrow.setOnClickListener(this);
+        btnShrink.setOnClickListener(this);
+        btnReset.setOnClickListener(this);
+        btnHide.setOnClickListener(this);
+        btnColor.setOnClickListener(this);
+
+
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view) {
+        // A local variable to use later
+        float size;
+
+        switch(view.getId()){
+            case R.id.btnAdd:
+                value ++;
+                txtValue.setText(""+ value);
+
+                break;
+
+            case R.id.btnTake:
+                value--;
+                txtValue.setText(""+ value);
+
+                break;
+
+            case R.id.btnReset:
+                value = 0;
+                txtValue.setText(""+ value);
+
+                break;
+
+            case R.id.btnGrow:
+                size = txtValue.getTextScaleX();
+                txtValue.setTextScaleX(size + 1);
+
+                break;
+
+            case R.id.btnShrink:
+                size = txtValue.getTextScaleX();
+                txtValue.setTextScaleX(size - 1);
+
+                break;
+
+            case R.id.btnHide:
+                if(txtValue.getVisibility() == View.VISIBLE)
+                {
+                    // Currently visible so hide it
+                    txtValue.setVisibility(View.INVISIBLE);
+
+                    // Change text on the button
+                    btnHide.setText("SHOW");
+
+                }else{
+                    // Currently hidden so show it
+                    txtValue.setVisibility(View.VISIBLE);
+
+                    // Change text on the button
+                    btnHide.setText("HIDE");
+                }
+                break;
+
+                case R.id.btnColor:
+                    Random rnd = new Random();
+                    int color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+                    txtValue.setTextColor(color);
+
+                    color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+                    view.setBackgroundColor(color);
+
+                    color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+                    test.setBackgroundColor(color);
+
+                    color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+                    btnAdd.setBackgroundColor(color);
+
+
+                    break;
+
+        }
+
+    }
+}
